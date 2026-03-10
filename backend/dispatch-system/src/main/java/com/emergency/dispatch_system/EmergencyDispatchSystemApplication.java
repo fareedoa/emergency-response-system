8e0cf0bbdb778c9203050ae7873025e1bcbc7f46
fareedoa <mysterokhidjhnr020@gmail.com>
2026-03-10 19:32:15 +0000

    adding backend initializer

---
+++ backend/dispatch-system/src/main/java/com/emergency/dispatch_system/EmergencyDispatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.emergency.dispatch_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyDispatchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyDispatchSystemApplication.class, args);
+	}
+
+}
---
+++ backend/dispatch-system/src/test/java/com/emergency/dispatch_system/EmergencyDispatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emergency.dispatch_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyDispatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
